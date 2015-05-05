941b9b2a97a58f204154747e76610a901e30a338
cody1991 <476490767@qq.com>
2015-05-05 14:13:08 +0800

    change

--- pageScroll/invitation/js/607/info_nomore.js
+++
@@ -1,9 +0,0 @@
- // 行为数据统计
-$("div[data-statis='info_nomore']").bind('click touchstart', function() {
-    var activity_id = $('#activity_id').html();
-    $.getJSON('/analyseplugin/plugin', {'activity_id': activity_id,'plugtype': 'info_nomore'}, function(response) {
-        if (response.success) {
-        // 统计成功, do none thing
-        }
-    });
-});
